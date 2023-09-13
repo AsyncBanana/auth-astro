7c98a51face1f3ed9e5b6e71bb836af9fac0bfc1
asyncbanana <jej.jackson.08@gmail.com>
2023-09-13 11:58:21 -0400

    refactor: use exports

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
+/// <reference path="module.d.ts"/>
 import integration from './src/integration'
 
 export type Integration = typeof integration
 export default integration
-export { defineConfig } from './src/config'
+export { defineConfig, type FullAuthConfig as Config } from './src/config'
--- module.d.ts
+++ module.d.ts
@@ -2,10 +2,3 @@ declare module 'auth:config' {
 	const config: import('./src/config').FullAuthConfig
 	export default config
 }
-
-declare module '@convocomet/auth-astro' {
-	const index: import('./index').Integration
-	const defineConfig: (config: import('./src/config').FullAuthConfig) => ConfigEnv
-	export default index
-	export { defineConfig }
-}
